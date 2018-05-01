dcdd106d7c3b2e01551a68b0cd7f371c741b2df1
RamSaw <mikhail@pravilov.com>
2018-05-01 15:23:18 +0300

    Corrected tests.

--- hw_13/src/test/java/com/mikhail/pravilov/mit/ftp/client/FtpClientTest.java
+++ hw_13/src/test/java/com/mikhail/pravilov/mit/ftp/client/FtpClientTest.java
@@ -4,6 +4,9 @@ import com.mikhail.pravilov.mit.ftp.server.FtpServer;
 import org.junit.Test;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 import static java.lang.Thread.sleep;
 import static org.junit.Assert.*;
@@ -11,9 +14,10 @@ import static org.junit.Assert.*;
 public class FtpClientTest {
     @Test
     public void main() throws Exception {
-        String filePath = new File("testData/testFileForGet").getAbsolutePath();
-        String dirPath = new File("testData").getAbsolutePath();
-        ByteArrayInputStream in = new ByteArrayInputStream(("list " + dirPath + "\nget " + filePath + "\n" + "exit\n").getBytes());
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("testFileForGet")).getFile());
+        File dir = new File("src/test/resources");
+        ByteArrayInputStream in = new ByteArrayInputStream(("list " + dir.getAbsolutePath() + "\nget " + file.getAbsolutePath() + "\n" + "exit\n").getBytes());
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         PrintStream outPrintStream = new PrintStream(out, true, "UTF-8");
         System.setIn(in);
@@ -21,9 +25,10 @@ public class FtpClientTest {
         new Thread(() -> FtpServer.main(new String[]{"8989"})).start();
         sleep(1000);
         FtpClient.main(new String[]{"localhost", "8989"});
-        String expectedOutput = "2\ntestFileForGet FALSE\ntestDir TRUE\n4\n74 65 73 74 \n";
-        assertEquals(expectedOutput, out.toString());
-        System.setOut(System.out);
+        String expectedOutput1 = "2\ntestFileForGet FALSE\ntestDir TRUE\n4\n74 65 73 74 \n";
+        String expectedOutput2 = "2\ntestDir TRUE\ntestFileForGet FALSE\n4\n74 65 73 74 \n";
+        assertTrue(expectedOutput1.equals(out.toString()) || expectedOutput2.equals(out.toString()));
+        System.setIn(System.in);
         System.setIn(System.in);
     }
 }
--- hw_13/src/test/java/com/mikhail/pravilov/mit/ftp/server/FtpProtocolTest.java
+++ hw_13/src/test/java/com/mikhail/pravilov/mit/ftp/server/FtpProtocolTest.java
@@ -3,15 +3,45 @@ package com.mikhail.pravilov.mit.ftp.server;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.Objects;
 
 import static org.junit.Assert.*;
 
 public class FtpProtocolTest {
     @Test
+    public void processList() throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        FtpProtocol ftpProtocol = new FtpProtocol(new DataOutputStream(out));
+        File dir = new File("src/test/resources");
+        ftpProtocol.process("list " + dir.getAbsolutePath());
+        DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(out.toByteArray()));
+        assertEquals(2, dataInputStream.readInt());
+        String read = dataInputStream.readUTF();
+        switch (read) {
+            case "testFileForGet":
+                assertEquals("testFileForGet", read);
+                assertFalse(dataInputStream.readBoolean());
+                assertEquals("testDir", dataInputStream.readUTF());
+                assertTrue(dataInputStream.readBoolean());
+                break;
+            case "testDir":
+                assertEquals("testDir", read);
+                assertTrue(dataInputStream.readBoolean());
+                assertEquals("testFileForGet", read);
+                assertFalse(dataInputStream.readBoolean());
+                break;
+            default:
+                assertTrue(read.equals("testFileForGet") || read.equals("testDir"));
+        }
+        assertEquals(-1, dataInputStream.read());
+    }
+
+    @Test
     public void processGet() throws Exception {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         FtpProtocol ftpProtocol = new FtpProtocol(new DataOutputStream(out));
-        File file = new File("testData/testFileForGet");
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("testFileForGet")).getFile());
         ftpProtocol.process("get " + file.getAbsolutePath());
         DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(out.toByteArray()));
         assertEquals(4, dataInputStream.readLong());
@@ -19,23 +49,6 @@ public class FtpProtocolTest {
         assertEquals(101, dataInputStream.readByte());
         assertEquals(115, dataInputStream.readByte());
         assertEquals(116, dataInputStream.readByte());
-        assertEquals(-1, dataInputStream.read());
-    }
-
-    @Test
-    public void processList() throws Exception {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        FtpProtocol ftpProtocol = new FtpProtocol(new DataOutputStream(out));
-
-        String fileName = "testData";
-        File file = new File(fileName);
-        ftpProtocol.process("list " + file.getAbsolutePath());
-        DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(out.toByteArray()));
-        assertEquals(2, dataInputStream.readInt());
-        assertEquals("testFileForGet", dataInputStream.readUTF());
-        assertFalse(dataInputStream.readBoolean());
-        assertEquals("testDir", dataInputStream.readUTF());
-        assertTrue(dataInputStream.readBoolean());
         assertEquals(-1, dataInputStream.read());
     }
 }
